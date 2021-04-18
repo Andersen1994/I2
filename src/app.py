f967aafc0a958db319c2f7b5d47c4e7ea984e1ba
Andersen1994 <ac941230@gmail.com>
2021-04-17 20:39:50 -0700

    add config

--- src/app.py
+++ src/app.py
@@ -1,6 +1,29 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv, find_dotenv
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    config = get_config()
+    secret = get_secret()
+    return f'Hello, World!\n Config: {config}\n Secret: {secret}'
+
+def get_config():
+    try:
+        load_dotenv(find_dotenv())
+        config = os.environ.get('CUSTOM_CONFIG')
+        return f'config: {config}'
+    except Exception as inst:
+        print(inst)
+        return 'config not found!'
+
+def get_secret():
+    try:
+        load_dotenv(find_dotenv())
+        config = os.environ.get('CUSTOM_SECRET')
+        return f'secret: {config}'
+    except Exception as inst:
+        print(inst)
+        return 'config not found!'
